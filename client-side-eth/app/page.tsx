0b3e08beeae9d20b51a05dc89a5d29d318e30f1f
sdt03 <dateraoshoumik@gmail.com>
2025-11-06 15:15:03 +0530

    changes

--- client-side-eth/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ReadBalance from "@/components/readonly";
-
-export default function ClientSideEth(){
-  return (
-    <div>
-      <ReadBalance />
-    </div>
-  )
-}
--- client-side-eth/lib/config.ts
+++
@@ -1,9 +0,0 @@
-import { http, createConfig } from "wagmi"
-import { mainnet } from "viem/chains"
-
-export const config = createConfig({
-    chains: [mainnet],
-    transports: {
-        [mainnet.id]: http()
-    }
-})
